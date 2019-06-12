066fafec0111cd5fa9d2dadb7927a0d3e3c15f7f
Sorush <sorushn@gmail.com>
2019-06-12 17:28:13 +0200

    pylint

--- camera_test.py
+++ camera_test.py
@@ -1,27 +1,26 @@
 import numpy as np
 import cv2
-from matplotlib import pyplot as plt
 
 cap1 = cv2.VideoCapture(0)
 cap2 = cv2.VideoCapture(2)
 while(True):
-    
-	_, imgL = cap1.read()
-	_, imgR = cap2.read()
-	
-	if (imgL is None) or (imgR is None):
-		continue
-	imgR = cv2.cvtColor(imgR, cv2.COLOR_BGR2GRAY)
-	imgR = cv2.flip(imgR,0)
-	imgL = cv2.cvtColor(imgL, cv2.COLOR_BGR2GRAY)
-	imgL = cv2.flip(imgL,0)
-	# disparity = stereo.compute(imgL, imgR).astype(np.float32) / 16.0
-	# disparity = (disparity-min_disp)/num_disp
-	# plt.imshow(disparity,'gray')
-	# plt.show()
-	cv2.imshow('camera feed', np.concatenate((imgL,imgR),axis=1))
-	if cv2.waitKey(1) & 0xFF == ord('q'):
-		break
+
+    _, imgL = cap1.read()
+    _, imgR = cap2.read()
+
+    if (imgL is None) or (imgR is None):
+        continue
+    imgR = cv2.cvtColor(imgR, cv2.COLOR_BGR2GRAY)
+    imgR = cv2.flip(imgR, 0)
+    imgL = cv2.cvtColor(imgL, cv2.COLOR_BGR2GRAY)
+    imgL = cv2.flip(imgL, 0)
+    # disparity = stereo.compute(imgL, imgR).astype(np.float32) / 16.0
+    # disparity = (disparity-min_disp)/num_disp
+    # plt.imshow(disparity,'gray')
+    # plt.show()
+    cv2.imshow('camera feed', np.concatenate((imgL, imgR), axis=1))
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
 
 cap1.release()
 cap2.release()
--- manual_calibration.py
+++ manual_calibration.py
@@ -1,6 +1,5 @@
 import cv2
 from matplotlib import pyplot as plt
-from PIL import Image
 import numpy as np
 
 #Measured Values
@@ -8,46 +7,46 @@ dX = 500
 dY = 350
 dZ = 570 #mm
 dx = 337
-dy = 240 #px 
+dy = 240 #px
 
 # Compute Intrinsics Matrix from measured values
-fx = dx*dZ/dX 
+fx = dx*dZ/dX
 fy = dy*dZ/dY
 K = np.diag([fx, fy, 1])
 K[0, 2] = 0.5*640
 K[1, 2] = 0.5*480
 
 
-def calibrate(image_size):
-	row, col = image_size
-	fx = FX*col/640
-	fy = FY*row/480
-	K = diag([fx,fy,1])
-	K[0,2] = 0.5*col
-	K[1,2] = 0.5*row
-	return K 
+# def calibrate(image_size):
+#     row, col = image_size
+#     fx = dX*col/640
+#     fy = dY*row/480
+#     K = np.diag([fx, fy, 1])
+#     K[0, 2] = 0.5*col
+#     K[1, 2] = 0.5*row
+#     return K 
 
 def measure_object_image():
-	img = cv2.imread('manual_calibration.png')
-	plt.imshow(img, 'gray')
-	
-	# select 2 opposite corners of the rectangular object
-	pts = np.asarray(plt.ginput(2,0))
-	print(pts[1]-pts[0])
+    img = cv2.imread('manual_calibration.png')
+    plt.imshow(img, 'gray')
+    
+    # select 2 opposite corners of the rectangular object
+    pts = np.asarray(plt.ginput(2, 0))
+    print(pts[1]-pts[0])
 
 def capture():
-	# capture an image of a rectangular object roughly at the center of the camera's view. Press C to capture.
-	cap1 = cv2.VideoCapture(2)
-	while(True):
-		_, img = cap1.read()
-		if img is None:
-			continue
-		img = cv2.flip(img,0)
-		cv2.imshow('camera feed', img)
-		c = cv2.waitKey(1)
-		if c & 0xFF == ord('c'):
-			cv2.imwrite("manual_calibration.png", img)
-		if c & 0xFF == ord('q'):
-			break
-	cap1.release()
-	cv2.destroyAllWindows()
+    # capture an image of a rectangular object roughly at the center of the camera's view. Press C to capture.
+    cap1 = cv2.VideoCapture(2)
+    while True:
+        _, img = cap1.read()
+        if img is None:
+            continue
+        img = cv2.flip(img,0)
+        cv2.imshow('camera feed', img)
+        c = cv2.waitKey(1)
+        if c & 0xFF == ord('c'):
+            cv2.imwrite("manual_calibration.png", img)
+        if c & 0xFF == ord('q'):
+            break
+    cap1.release()
+    cv2.destroyAllWindows()
